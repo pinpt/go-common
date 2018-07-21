bf93a5aec7dfe2d419b38ca38981f4083e8e4dfc
Jeff Haynie <jhaynie@pinpt.com>
2018-07-20 19:39:39 -0700

    fixed issue with post, added initial tests, better documentation

--- api/api.go
+++ api/api.go
@@ -18,20 +18,29 @@ import (
 	"github.com/pinpt/httpclient"
 )
 
-// https://www.amazontrust.com/repository/
-// these are the only Certificate Authorities we trust for our certificate
-var caWeTrust = map[string]bool{
-	"fbe3018031f9586bcbf41727e417b7d1c45c2f47f93be372a17b96b50757d5a2": true, //CN=Amazon Root CA 1,O=Amazon,C=US
-	"7f4296fc5b6a4e3b35d3c369623e364ab1af381d8fa7121533c9d6c633ea2461": true, //CN=Amazon Root CA 2,O=Amazon,C=US
-	"36abc32656acfc645c61b71613c4bf21c787f5cabbee48348d58597803d7abc9": true, //CN=Amazon Root CA 3,O=Amazon,C=US
-	"f7ecded5c66047d28ed6466b543c40e0743abe81d109254dcf845d4c2c7853c5": true, //CN=Amazon Root CA 4,O=Amazon,C=US
-	"b58539ecaa13921ccdb80d38d34875fde6471c5a159d9beef2fa6f99983bd611": true, //CN=Amazon,OU=Server CA 0A,O=Amazon,C=US
-	"64bb5bd80311fa3f53bd99404cab8762ad6e048447d0a97f219fceca1661f37c": true, //CN=Amazon,OU=Server CA 1A,O=Amazon,C=US
-	"1bda5afff83781380bf708198f9f7d2db1e067f14bb9ea7bae70aac0b0305e7b": true, //CN=Amazon,OU=Server CA 2A,O=Amazon,C=US
-	"7c53b5df79135d9af6195ef6bb73ab98c2c89950b892b192dffd784db925a41c": true, //CN=Amazon,OU=Server CA 3A,O=Amazon,C=US
-	"17708dff2b7faec9cb1b5215ebb2421d97b0543c936fac9d6e02b92f20e5c707": true, //CN=Amazon,OU=Server CA 4A,O=Amazon,C=US
-}
+// baseURL is the url to the Pinpoint Cloud API
+const baseURL = "api.pinpt.io"
 
+var (
+	// domainRegex are the only domains that we trust
+	domainRegex = regexp.MustCompile("\\.(pinpt|ppoint)\\.(net|com|io|co)$")
+
+	// caWeTrust are the only Certificate Authorities we trust for our certificate
+	// see list from Amazon AwS at https://www.amazontrust.com/repository/
+	caWeTrust = map[string]bool{
+		"fbe3018031f9586bcbf41727e417b7d1c45c2f47f93be372a17b96b50757d5a2": true, //CN=Amazon Root CA 1,O=Amazon,C=US
+		"7f4296fc5b6a4e3b35d3c369623e364ab1af381d8fa7121533c9d6c633ea2461": true, //CN=Amazon Root CA 2,O=Amazon,C=US
+		"36abc32656acfc645c61b71613c4bf21c787f5cabbee48348d58597803d7abc9": true, //CN=Amazon Root CA 3,O=Amazon,C=US
+		"f7ecded5c66047d28ed6466b543c40e0743abe81d109254dcf845d4c2c7853c5": true, //CN=Amazon Root CA 4,O=Amazon,C=US
+		"b58539ecaa13921ccdb80d38d34875fde6471c5a159d9beef2fa6f99983bd611": true, //CN=Amazon,OU=Server CA 0A,O=Amazon,C=US
+		"64bb5bd80311fa3f53bd99404cab8762ad6e048447d0a97f219fceca1661f37c": true, //CN=Amazon,OU=Server CA 1A,O=Amazon,C=US
+		"1bda5afff83781380bf708198f9f7d2db1e067f14bb9ea7bae70aac0b0305e7b": true, //CN=Amazon,OU=Server CA 2A,O=Amazon,C=US
+		"7c53b5df79135d9af6195ef6bb73ab98c2c89950b892b192dffd784db925a41c": true, //CN=Amazon,OU=Server CA 3A,O=Amazon,C=US
+		"17708dff2b7faec9cb1b5215ebb2421d97b0543c936fac9d6e02b92f20e5c707": true, //CN=Amazon,OU=Server CA 4A,O=Amazon,C=US
+	}
+)
+
+// isTrusted returns true if the Certificate is a CA Certificate that we explicitly trust
 func isTrusted(cert *x509.Certificate) bool {
 	der, _ := x509.MarshalPKIXPublicKey(cert.PublicKey)
 	hash := sha256.Sum256(der)
@@ -39,9 +48,7 @@ func isTrusted(cert *x509.Certificate) bool {
 	return caWeTrust[fingerprint]
 }
 
-// these are the only domains that we trust
-var domainRegex = regexp.MustCompile("\\.(pinpt|ppoint)\\.(net|com|io|co)$")
-
+// isDNSNameTrusted returns true if the DNS name is a domain we explicitly trust
 func isDNSNameTrusted(names ...string) bool {
 	for _, name := range names {
 		if domainRegex.MatchString(name) {
@@ -50,8 +57,6 @@ func isDNSNameTrusted(names ...string) bool {
 	}
 	return false
 }
-
-const baseURL = "api.pinpt.io"
 
 // BackendURL return the base url to the API server
 func BackendURL(channel string) string {
@@ -118,12 +123,25 @@ func NewHTTPAPIClientDefault() (httpclient.Client, error) {
 	return NewHTTPAPIClient(hcConfig)
 }
 
+// absurl is absolute if matching our API url pattern
+var absurl = regexp.MustCompile("^https://(.*)" + baseURL + "$")
+
+func isAbsURL(urlstr string) bool {
+	return absurl.MatchString(urlstr)
+}
+
 // Get will invoke api for channel and basepath
 func Get(channel string, basepath string, apiKey string) (*http.Response, error) {
 	bu := BackendURL(channel)
-	u, _ := url.Parse(bu)
-	u.Path = basepath
-	req, _ := http.NewRequest(http.MethodGet, u.String(), nil)
+	var urlstr string
+	if isAbsURL(basepath) {
+		urlstr = basepath
+	} else {
+		u, _ := url.Parse(bu)
+		u.Path = basepath
+		urlstr = u.String()
+	}
+	req, _ := http.NewRequest(http.MethodGet, urlstr, nil)
 	if apiKey != "" {
 		req.Header.Add("x-api-key", apiKey)
 	}
@@ -142,14 +160,19 @@ func Get(channel string, basepath string, apiKey string) (*http.Response, error)
 // Post will invoke api for channel and basepath as JSON post
 func Post(channel string, basepath string, apiKey string, obj interface{}) (*http.Response, error) {
 	bu := BackendURL(channel)
-	u, _ := url.Parse(bu)
-	u.Path = basepath
-	req, _ := http.NewRequest(http.MethodGet, u.String(), strings.NewReader(json.Stringify(obj)))
+	var urlstr string
+	if isAbsURL(basepath) {
+		urlstr = basepath
+	} else {
+		u, _ := url.Parse(bu)
+		u.Path = basepath
+		urlstr = u.String()
+	}
+	req, _ := http.NewRequest(http.MethodPost, urlstr, strings.NewReader(json.Stringify(obj)))
 	if apiKey != "" {
 		req.Header.Add("x-api-key", apiKey)
 	}
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "application/json")
 	client, err := NewHTTPAPIClientDefault()
 	if err != nil {
 		return nil, err
---
+++ api/api_test.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAbsoluteURL(t *testing.T) {
+	assert := assert.New(t)
+	assert.True(isAbsURL("https://api.edge.api.pinpt.io"))
+	assert.True(isAbsURL("https://api.stable.api.pinpt.io"))
+	assert.False(isAbsURL("http://api.edge.api.pinpt.io"))
+	assert.False(isAbsURL("http://api.stable.api.pinpt.io"))
+	assert.False(isAbsURL("/foo"))
+	assert.False(isAbsURL("foo"))
+}
+
+func TestBackendURL(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal("https://api.edge.api.pinpt.io", BackendURL("edge"))
+	assert.Equal("https://api.stable.api.pinpt.io", BackendURL("stable"))
+}
+
+func TestBackendURLPing(t *testing.T) {
+	assert := assert.New(t)
+	resp, err := Get("edge", "/frontend/ping", "")
+	assert.NoError(err)
+	buf, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	assert.NoError(err)
+	assert.Equal("OK", string(buf))
+
+	resp, err = Get("stable", "/frontend/ping", "")
+	assert.NoError(err)
+	buf, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	assert.NoError(err)
+	assert.Equal("OK", string(buf))
+}
