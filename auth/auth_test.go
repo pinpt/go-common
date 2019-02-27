6bc9d4f6e33e5e6de428fa1a7ac637506acf97a3
Jeff Haynie <jhaynie@pinpt.com>
2019-02-26 20:00:19 -0600

    fixed test cases

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"fmt"
 	"net/url"
 	"strings"
 	"testing"
@@ -31,11 +30,10 @@ func TestAuth(t *testing.T) {
 	// s%3An62lbKqtNibwptxyy4oN8UB4cq2tFVzgwery2EHCnOtMDGFA%252BBYd%252FuKTMPOm9ZMuWOrZzQMckY9FkWLiFF%252BdBfuEZXrZ8vNHEcwOC96Yp1SNIT8vKQLBfdypEojWxJhw7jMb1QeGgCkFfPxV7CDlqXZV2Hthwz%252F3Tb08Sa%252BYggpOXw%253D%253D.Pn0%2FRF59XxTEZBPlFodTCEhgEDBl%2FU0jgdsMrHsDKo4
 	// s%3AhIcRXxzgFt9%252F9jYWmRqfc%252FZ3EXuz4cBISvTrGUDHoeyDzkLf4bJmqPxD68G7ocjwIPI4Tsb6d4oMKEPbH3c4653IsKI40aWFM%252Bwicp4BKCuD%252FogNkE%252BGVNHZiNMY42GUWLqY4fMjlANnyCT4occHG8x492a8IVT8aBkqXTmQVzNPMQ%253D%253D.PwC2MAiUYxpnqCe774lCNvZDQO2BamRxI33B4ox2JuE
 	esc, _ := url.PathUnescape("4sqHeZZYfHLOLjQC441U7%2F2sbZKrN3Nm13EpBT7F5GqDcuYv4mZSqDu1FqQ3kefMXEQzqr14RsO9J%2FwIPFyLK3pV7iT%2BRs047dGaW%2Frhb5MDhMpgZxhBNCtFbmN80Ihs9EaYfWKWQp7T1RhM8acGXkk1IiywYmQ2qomc3rpQgaKq%2Fw%3D%3D")
-	assert.Equal("P+m32ewamHkyE4JgDNGnNW18w/dQ/lKPxoQdprGCwF/SH0OTi/EiUF16sIeLOMtULJCDbwzSNL8mf+t/Nn4XoPN9Eu1Jj/ujijSWStXG3lZfg3FnisVc2rlDaHkFAQrZGpRlgxVOOvG18vq7TOHP1sTQZ6E9B7C6wsY05Q6gnajkLg==", esc)
-	fmt.Println(esc)
+	assert.Equal("4sqHeZZYfHLOLjQC441U7/2sbZKrN3Nm13EpBT7F5GqDcuYv4mZSqDu1FqQ3kefMXEQzqr14RsO9J/wIPFyLK3pV7iT+Rs047dGaW/rhb5MDhMpgZxhBNCtFbmN80Ihs9EaYfWKWQp7T1RhM8acGXkk1IiywYmQ2qomc3rpQgaKq/w==", esc)
 	dec2, err := DecryptString(esc, "^LB-f83tk^ceL866H2zj$M$jgvP3XHtc")
 	assert.NoError(err)
-	assert.Equal("{\"customer_id\":\"5500a5ba8135f296\",\"scopes\":[],\"expiry\":1547756915352,\"type\":\"session\"}", dec2)
+	assert.Equal("{\"customer_id\":\"5500a5ba8135f296\",\"scopes\":[],\"expiry\":1547757648610,\"type\":\"session\"}", dec2)
 }
 
 func TestAuthInvalid(t *testing.T) {
@@ -53,7 +51,7 @@ func TestCookieKeyValue(t *testing.T) {
 	indx1 := strings.Index(enc, ":") + 1
 	indx2 := strings.Index(enc, ".")
 	enc = enc[indx1:indx2]
-	assert.Equal("", enc)
+	assert.Equal("lqGI4NztuaWSoaucE2R8ltP+PkQl4asO431bdwSZPKOBnfPE/jdklX3xb2EQ0pYLdD387fMEt7l8XDRQ+UiSONirXIijNuMSHMH1Y1eJsvJzBKSmnpLOM/wK6/BvRUOgoLhWjK8ptQJCzW337PDq+SVSGXa0K6PoOrqqFzfT5jr8Ow==", enc)
 	dec, err := DecryptString(enc, "^LB-f83tk^ceL866H2zj$M$jgvP3XHtc")
 	assert.NoError(err)
 	assert.NotEmpty(dec)
