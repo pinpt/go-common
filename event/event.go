642c1c480c214f47e259bc42114483f454222a67
Jeff Haynie <jhaynie@pinpoint.com>
2019-05-24 09:40:25 -0500

    rework the event interface to better expose information

--- event/event.go
+++ event/event.go
@@ -2,14 +2,25 @@ package event
 
 import (
 	"context"
+	"time"
 
 	"github.com/linkedin/goavro"
 )
 
+// Message encapsulates data for the event system
+type Message struct {
+	Codec     *goavro.Codec
+	Key       string
+	Value     []byte
+	Headers   map[string]string
+	Timestamp time.Time
+	Extra     map[string]interface{}
+}
+
 // Producer will emit events to consumers
 type Producer interface {
 	// Send will send the event
-	Send(ctx context.Context, codec *goavro.Codec, key []byte, value []byte) error
+	Send(ctx context.Context, msg Message) error
 	// Close will close the producer
 	Close() error
 }
@@ -17,7 +28,7 @@ type Producer interface {
 // ConsumerCallback will receive events from producers
 type ConsumerCallback struct {
 	// OnDataReceived is called when an event is received
-	OnDataReceived func(key []byte, value []byte) error
+	OnDataReceived func(msg Message) error
 	// OnErrorReceived is called when an error is received
 	OnErrorReceived func(err error)
 }
