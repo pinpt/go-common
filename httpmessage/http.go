231ce880b9e80112b284c9edbefdf23989760ae9
Jeff Haynie <jhaynie@pinpoint.com>
2019-07-20 10:37:20 -0500

    add updated error page template

--- httpmessage/http.go
+++ httpmessage/http.go
@@ -10,6 +10,7 @@ import (
 type templateParams struct {
 	Title   string
 	Message string
+	Error   string
 }
 
 type jsonResponse struct {
@@ -18,7 +19,7 @@ type jsonResponse struct {
 }
 
 // RenderStatus will render an appropriate message detail
-func RenderStatus(w http.ResponseWriter, req *http.Request, status int, title, message string) {
+func RenderStatus(w http.ResponseWriter, req *http.Request, status int, title, msg string) {
 	w.WriteHeader(status)
 	w.Header().Set("Cache-Control", "max-age=0,no-cache,no-store")
 	accept := req.Header.Get("accept")
@@ -26,9 +27,15 @@ func RenderStatus(w http.ResponseWriter, req *http.Request, status int, title, m
 	rw := req.Header.Get("x-requested-with") == "XMLHttpRequest"
 	if strings.Contains(accept, "json") || rw || strings.Contains(ct, "json") {
 		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(pjson.Stringify(jsonResponse{false, message})))
+		w.Write([]byte(pjson.Stringify(jsonResponse{false, msg})))
 		return
 	}
 	w.Header().Set("Content-Type", "text/html")
-	tmpl.Execute(w, templateParams{title, message})
+	var err, message string
+	if status >= 500 {
+		err = msg
+	} else {
+		message = msg
+	}
+	tmpl.Execute(w, templateParams{title, message, err})
 }
