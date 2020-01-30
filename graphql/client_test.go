83644f71ffda22e2cb2433e9aff8ac7564289807
Pedro Enrique <penrique@pinpoint.com>
2020-01-30 19:24:51 +0100

    use mock server for testing

--- graphql/client_test.go
+++ graphql/client_test.go
@@ -1,11 +1,18 @@
 package graphql
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
 	"github.com/pinpt/go-common/hash"
 	"github.com/stretchr/testify/assert"
-	"math/rand"
-	"os"
-	"testing"
 )
 
 func TestClient(t *testing.T) {
@@ -15,20 +22,56 @@ func TestClient(t *testing.T) {
 	}
 	assert := assert.New(t)
 
-	// these need to be set
 	apikey := os.Getenv("PP_AUTH_SHARED_SECRET")
 	hostname := os.Getenv("PP_GRAPHQL_URL")
 
+	// if hostname is missing, use the mock server
+	if hostname == "" {
+		var closer func()
+		closer, hostname = mockServer()
+		defer closer()
+	}
 	client, err := NewClient(hash.Values("customer 0"), "", apikey, hostname)
 	assert.NoError(err)
 
 	id, err := createNewCustomer(client, "Dummy Customer", "localhost")
+
 	assert.NoError(err)
 	assert.NotEmpty(id)
 
 	deleted, err := deleteCustomer(client, id)
 	assert.NoError(err)
 	assert.True(deleted)
+}
+
+func mockServer() (closer func(), url string) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		var payload struct {
+			Variables Variables `json:"variables"`
+			Query     string    `json:"query"`
+		}
+
+		if err := json.Unmarshal(body, &payload); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		bodyString := strings.TrimSpace(payload.Query)
+		if strings.HasPrefix(bodyString, "mutation createNewCustomer") {
+			fmt.Fprintf(w, `{"data":{"admin":{"createCustomer":{"_id":"437384b8bacc1653"}}}}`)
+			return
+		}
+		if strings.HasPrefix(bodyString, "mutation deleteCustomer") {
+			fmt.Fprintf(w, `{"data":{"admin":{"deleteCustomer":true}}}`)
+			return
+		}
+		http.Error(w, fmt.Sprintf("response not handled, body: %s", body), http.StatusBadRequest)
+	}))
+	return ts.Close, ts.URL
 }
 
 func createNewCustomer(client Client, name string, hostname string) (_id string, _ error) {
